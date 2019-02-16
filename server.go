82ffee9c7a79f204411a5885c229590d29262827
Nick Huang <hcj1009x@gmail.com>
2019-02-16 07:47:44 -0500

    Add front end

--- server.go
+++ server.go
@@ -3,22 +3,44 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"encoding/json"
 )
 
-
 func main() {
-	frontend := http.FileServer(http.Dir("static"))
-	musicfs := http.FileServer(http.Dir("music"))
-	http.Handle("/static/", http.StripPrefix("/static/", frontend))
-	http.Handle("/music/", http.StripPrefix("/music/", musicfs))  
+	react := http.FileServer(http.Dir("app/build"))
+	storage := http.FileServer(http.Dir("music"))
+	http.Handle("/", http.StripPrefix("/", react))
+	http.Handle("/storage/", http.StripPrefix("/storage/", storage))
+
+	http.HandleFunc("/albums", handleAlbum)
+	http.HandleFunc("/songs", handleMusic)
 	http.HandleFunc("/upload", handleUpload)
+	
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
 }
 
+func handleAlbum(w http.ResponseWriter, r *http.Request) {
+	albums, _ := ioutil.ReadDir("storage/albums")
+	albumNames := list.List()
+	for _, album := range albums {
+		albumNames.PushBack(album.Name());
+	}
+	j, err := json.Marshal(albumNames)
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+	fmt.Fprintf(w, "%v", string(j))
+}
+
+func handleMusic(w http.ResponseWriter, r *http.Request) {
+
+}
 
 func handleUpload(w http.ResponseWriter, r *http.Request) {
 	fmt.Print("Uploading")
@@ -39,8 +61,6 @@ func handleUpload(w http.ResponseWriter, r *http.Request) {
 	}
 
 	defer f.Close()
-
 	io.Copy(f, file)
-
 }
 
